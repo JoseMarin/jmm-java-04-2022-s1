1c8976dac96d44cda7176df4623967dcbb35e84c
JoseMarin <jmarinmalagon@gamil.com>
2022-04-05 12:39:49 +0200

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,10 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Buenas tardes!!!");
+		System.out.println("Buenas tardes!!!");
+		System.out.println("Buenas tardes!!!");
+		System.out.println("Buenas tardes!!!");
+		
 	}
 
 }
