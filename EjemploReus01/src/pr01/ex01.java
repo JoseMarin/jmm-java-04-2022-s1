db80999d7a3a75ccc892802e6c7683d3c4656c42
JoseMarin <jmarinmalagon@gamil.com>
2022-04-05 12:33:51 +0200

    Mi primer proyecto Java 1.08

---
+++ EjemploReus01/src/pr01/ex01.java
@@ -0,0 +1,10 @@
+package pr01;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Pr01");
+	}
+
+}
---
+++ EjemploReus01/src/pr02/ex02.java
@@ -0,0 +1,10 @@
+package pr02;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Pr02");
+	}
+
+}
